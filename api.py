d2891238c04056037c7d76ec9bbb5996b62e4c45
Михаил Капелько <kornerr@gmail.com>
2022-06-20 16:17:05 +0300

    поправить выдачу изображения для PythonAnywhere | fix image publishing for PythonAnywhere

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 import flask
+import io
 import os.path
+import werkzeug.wsgi
 
 app = flask.Flask(__name__)
 
@@ -24,7 +26,10 @@ def system_info():
 
 @app.route("/resource/<string:id>")
 def resource(id):
+    // https://www.pythonanywhere.com/forums/topic/13570/
     filename = "static/{0}.jpg".format(id)
-    if os.path.isfile(filename):
-      return flask.send_file(filename, mimetype="image/jpg")
+    with open(filename, "rb") as f:
+        b = io.BytesIO(f.read())
+        w = werkzeug.wsgi.FileWrapper(b)
+        return flask.Response(w, mimetype = "image/jpg", direct_passthrough = True)
     return flask.abort(404)
