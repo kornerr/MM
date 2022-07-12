3494084a3b4d35d4f82088a9cd367fe12a76aef4
Михаил Капелько <kornerr@gmail.com>
2022-07-12 18:48:55 +0300

    d

--- MaVC/src/LoginViewController.swift
+++ MaVC/src/LoginViewController.swift
@@ -1,10 +1,25 @@
+import Anchorage
 import UIKit
 
 class LoginViewController: UIViewController {
+  let form = UIView()
+  let headerTitle = UILabel()
 
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .white
+    setupUI()
+  }
+
+  private func setupUI() {
+    view.addSubview(form)
+    form.centerAnchors /==/ view.centerAnchors
+
+    view.addSubview(headerTitle)
+    headerTitle.leftAnchor /==/ view.leftAnchor + 8
+    headerTitle.rightAnchor /==/ view.rightAnchor - 8
+    headerTitle.textAlignment = .center
+    headerTitle.text = "🎃 Murk in Models 🎃";
   }
 }
 
