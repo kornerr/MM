7ca9d92bb273826b8ad8153cff85ef0c5b2e7fa6
Михаил Капелько <kornerr@gmail.com>
2022-08-16 19:42:01 +0300

    d

--- MM/mod/LoginUI/src/LoginUI.V.swift
+++ MM/mod/LoginUI/src/LoginUI.V.swift
@@ -90,7 +90,6 @@ extension LoginUI.V {
           .scaledToFill()
           .frame(width: 100, height: 100)
           .clipShape(Circle())
-          /**/.border(.red)
           .offset(y: -220)
       }
     }
