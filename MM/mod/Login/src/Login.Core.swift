ae3ae21d05dfc64c708dc9be50820ad747c0792b
Михаил Капелько <kornerr@gmail.com>
2022-08-17 16:15:43 +0300

    d

--- MM/mod/Login/src/Login.Core.swift
+++ MM/mod/Login/src/Login.Core.swift
@@ -24,89 +24,101 @@ extension Login {
           resultSystemInfo.eraseToAnyPublisher(),
           vm.$username.removeDuplicates().eraseToAnyPublisher()
         )
-
-      ui.content = UIHostingController(rootView: LoginUI.V(vm))
-      vm.hostLabel = "Host"
-      vm.passwordLabel = "Password"
-      vm.usernameLabel = "Username"
-      vm.version = "Version: MM-1"
-
-      // Форматируем поле host.
-      ctrl.m
-        .compactMap { $0.shouldResetHost }
-        .receive(on: DispatchQueue.main)
-        .sink { [weak self] v in self?.vm.host = v }
-        .store(in: &subscriptions)
-
-      // Форматируем поле password.
-      ctrl.m
-        .compactMap { $0.shouldResetPassword }
-        .receive(on: DispatchQueue.main)
-        .sink { [weak self] v in self?.vm.password = v }
-        .store(in: &subscriptions)
-
-      // Форматируем поле username.
-      ctrl.m
-        .compactMap { $0.shouldResetUsername }
-        .receive(on: DispatchQueue.main)
-        .sink { [weak self] v in self?.vm.username = v }
-        .store(in: &subscriptions)
-
-      // Загружаем информацию о системе при смене хоста.
-      ctrl.m
-        .map { $0.shouldRefreshSystemInfo }
-        .removeDuplicates()
-        .debounce(for: .seconds(0.3), scheduler: DispatchQueue.main)
-        .flatMap { [weak self] url -> AnyPublisher<Net.SystemInfo?, Never> in
-          guard let url = url else { return Just(nil).eraseToAnyPublisher() }
-          self?.isLoadingSystemInfo.send()
-          return URLSession.shared.dataTaskPublisher(for: url)
-            .map { v in try? JSONDecoder().decode(Net.SystemInfo.self, from: v.data) }
-            .catch { _ in Just(nil) }
-            .eraseToAnyPublisher()
-        }
-        .receive(on: DispatchQueue.main)
-        .sink { [weak self] info in self?.resultSystemInfo.send(info) }
-        .store(in: &subscriptions)
-
-      // Загружаем логотип хоста.
-      ctrl.m
-        .map { $0.shouldRefreshHostLogo }
-        .removeDuplicates()
-        .flatMap { [weak self] url -> AnyPublisher<UIImage?, Never> in
-          guard let url = url else { return Just(nil).eraseToAnyPublisher() }
-          return URLSession.shared.dataTaskPublisher(for: url)
-            .map { v in UIImage(data: v.data) }
-            .catch { _ in Just(nil) }
-            .eraseToAnyPublisher()
-        }
-        .receive(on: DispatchQueue.main)
-        .sink { [weak self] img in self?.resultHostLogo.send(img) }
-        .store(in: &subscriptions)
-
-      // Отображаем имя хоста.
-      ctrl.m
-        .map { $0.shouldResetHostName }
-        .removeDuplicates()
-        .receive(on: DispatchQueue.main)
-        .sink { [weak self] v in self?.vm.hostName = v }
-        .store(in: &subscriptions)
-
-      // Отображаем загрузку системной инфы.
-      ctrl.m
-        .map { $0.isLoadingSystemInfo }
-        .removeDuplicates()
-        .receive(on: DispatchQueue.main)
-        .sink { [weak self] v in self?.vm.isLoadingSystemInfo = v }
-        .store(in: &subscriptions)
-
-      // Отображаем логотип хоста.
-      ctrl.m
-        .map { $0.hostLogo }
-        .removeDuplicates()
-        .receive(on: DispatchQueue.main)
-        .sink { [weak self] img in self?.vm.hostLogo = img }
-        .store(in: &subscriptions)
+      setupPipes()
+      setupUI()
+      setupNetwork()
     }
+  }
+}
+
+extension Login.Core {
+  private func setupNetwork() {
+    // Загружаем информацию о системе при смене хоста.
+    ctrl.m
+      .map { $0.shouldRefreshSystemInfo }
+      .removeDuplicates()
+      .debounce(for: .seconds(0.3), scheduler: DispatchQueue.main)
+      .flatMap { [weak self] url -> AnyPublisher<Net.SystemInfo?, Never> in
+        guard let url = url else { return Just(nil).eraseToAnyPublisher() }
+        self?.isLoadingSystemInfo.send()
+        return URLSession.shared.dataTaskPublisher(for: url)
+          .map { v in try? JSONDecoder().decode(Net.SystemInfo.self, from: v.data) }
+          .catch { _ in Just(nil) }
+          .eraseToAnyPublisher()
+      }
+      .receive(on: DispatchQueue.main)
+      .sink { [weak self] info in self?.resultSystemInfo.send(info) }
+      .store(in: &subscriptions)
+
+    // Загружаем логотип хоста.
+    ctrl.m
+      .map { $0.shouldRefreshHostLogo }
+      .removeDuplicates()
+      .flatMap { [weak self] url -> AnyPublisher<UIImage?, Never> in
+        guard let url = url else { return Just(nil).eraseToAnyPublisher() }
+        return URLSession.shared.dataTaskPublisher(for: url)
+          .map { v in UIImage(data: v.data) }
+          .catch { _ in Just(nil) }
+          .eraseToAnyPublisher()
+      }
+      .receive(on: DispatchQueue.main)
+      .sink { [weak self] img in self?.resultHostLogo.send(img) }
+      .store(in: &subscriptions)
+  }
+
+  private func setupPipes() {
+  }
+
+  private func setupUI() {
+    ui.content = UIHostingController(rootView: LoginUI.V(vm))
+    vm.hostLabel = "Host"
+    vm.passwordLabel = "Password"
+    vm.usernameLabel = "Username"
+    vm.version = "Version: MM-1"
+
+    // Форматируем поле host.
+    ctrl.m
+      .compactMap { $0.shouldResetHost }
+      .receive(on: DispatchQueue.main)
+      .sink { [weak self] v in self?.vm.host = v }
+      .store(in: &subscriptions)
+
+    // Форматируем поле password.
+    ctrl.m
+      .compactMap { $0.shouldResetPassword }
+      .receive(on: DispatchQueue.main)
+      .sink { [weak self] v in self?.vm.password = v }
+      .store(in: &subscriptions)
+
+    // Форматируем поле username.
+    ctrl.m
+      .compactMap { $0.shouldResetUsername }
+      .receive(on: DispatchQueue.main)
+      .sink { [weak self] v in self?.vm.username = v }
+      .store(in: &subscriptions)
+
+    // Отображаем имя хоста.
+    ctrl.m
+      .map { $0.shouldResetHostName }
+      .removeDuplicates()
+      .receive(on: DispatchQueue.main)
+      .sink { [weak self] v in self?.vm.hostName = v }
+      .store(in: &subscriptions)
+
+    // Отображаем загрузку системной инфы.
+    ctrl.m
+      .map { $0.isLoadingSystemInfo }
+      .removeDuplicates()
+      .receive(on: DispatchQueue.main)
+      .sink { [weak self] v in self?.vm.isLoadingSystemInfo = v }
+      .store(in: &subscriptions)
+
+    // Отображаем логотип хоста.
+    ctrl.m
+      .map { $0.hostLogo }
+      .removeDuplicates()
+      .receive(on: DispatchQueue.main)
+      .sink { [weak self] img in self?.vm.hostLogo = img }
+      .store(in: &subscriptions)
   }
 }
